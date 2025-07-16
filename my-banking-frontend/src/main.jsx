ced0b55bac4882eb3e065ab61b420008de8c52c7
Lachezar Dimitrov <lachezardimitrov@Lachezars-MacBook-Air.local>
2025-07-16 22:44:08 +0300

    Refactor: responsive UI/UX, env vars, Honeybadger error monitoring, docs update

--- my-banking-frontend/src/main.jsx
+++ my-banking-frontend/src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Honeybadger from '@honeybadger-io/js';
+
+Honeybadger.configure({
+  apiKey: import.meta.env.VITE_HONEYBADGER_API_KEY,
+  environment: import.meta.env.MODE,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
